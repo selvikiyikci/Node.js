4b2f23b9e1fd744a33268cd31a4857eda792d7e7
selvikiyikci <selvikiyikci@gmail.com>
2024-07-14 16:53:07 +0300

    added a  POST /create route to append to blogs.txt and redirect to home

--- 3-file-system.js
+++ 3-file-system.js
@@ -17,6 +17,29 @@ var server = http.createServer((request, response) => {
         response.end();
     });
 }
+
+else if (request.url == "/create" && request.method == "POST") {
+    fs.appendFile("blogs.txt","deneme", (err) =>{
+        if(err){
+            console.log(err);
+        }
+        else {
+            response.statusCode = 302;
+            response.setHeader("Location", "/");
+            response.end();
+        }
+ 
+});
+}
+
+else if (request.url == "/create") {
+    fs.readFile("create.html", (error, html) =>{
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(html);
+    response.end();
+});
+}
+
     else {
         fs.readFile("404.html", (error, html) =>{
         response.writeHead(404, { "Content-Type": "text/html" });
