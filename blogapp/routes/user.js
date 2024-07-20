a396e03d39be6fcbb61dab12dea6e78ca72cb0a0
selvikiyikci <selvikiyikci@gmail.com>
2024-07-20 23:19:47 +0300

    updated with a category section and pages.

--- blogapp/routes/user.js
+++ blogapp/routes/user.js
@@ -41,15 +41,33 @@ const data = {
         },
     ]
 }
+router.use("/blogs/category/:categoryid", async function(req, res){
+    const id = req.params.categoryid;
+    try{
+        const [blogs, ] = await db.execute("select * from blog where categoryid=?", [id]);
+        const [categories, ]= await db.execute("select * from category");
+        res.render("users/blogs", {
+            title : "Tüm Kurslar",
+            blogs : blogs,
+            categories : categories,
+            selectedCategory : id
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 
 router.use("/blogs/:blogid", async function(req, res) {
     const id = req.params.blogid;
     try {
-        const [blog,] = await db.execute("select * from blog where blogid = ?", [id]);
-        if(blog[0]){
+        const [blogs,] = await db.execute("select * from blog where blogid = ?", [id]);
+        const blog = blogs[0];
+        if(blog){
             return res.render("users/blog-details", {
-            title : blog[0].baslik,
-            blog : blog[0]
+            title : blog.baslik,
+            blog : blog,
+            selectedCategory : null
             
         });
      }
@@ -67,7 +85,8 @@ router.use("/blogs", async function(req, res) {
         res.render("users/blogs", {
             title : "Tüm Kurslar",
             blogs : blogs,
-            categories : categories
+            categories : categories,
+            selectedCategory : null
 
         })
     }
@@ -86,7 +105,8 @@ router.use("/", async function(req, res) {
         res.render("users/index", {
             title : "Popüler Kurslar",
             blogs : blogs,
-            categories : categories
+            categories : categories,
+            selectedCategory : null
 
         })
     }
