6e43d39266fee6aaf7525ad3b06f24604cec2a9a
selvikiyikci <selvikiyikci@gmail.com>
2024-07-20 16:33:27 +0300

    added mysql connections and a title

--- blogapp/routes/user.js
+++ blogapp/routes/user.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
+
+const db = require("../data/db");
+
 const data = {
     title: "Popüler Kurslar",
     categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
@@ -39,27 +42,60 @@ const data = {
     ]
 }
 
+router.use("/blogs/:blogid", async function(req, res) {
+    const id = req.params.blogid;
+    try {
+        const [blog,] = await db.execute("select * from blog where blogid = ?", [id]);
+        if(blog[0]){
+            return res.render("users/blog-details", {
+            title : blog[0].baslik,
+            blog : blog[0]
+            
+        });
+     }
+     res.redirect("/");
+    }
+     catch(err){
+        console.log(err);
+     }
+});
 
-router.use("/blogs/:blogid", function (req, res) {
-    res.sendFile(path.join(__dirname, "../views/users", "blog-details.html"));
+router.use("/blogs", async function(req, res) {
+    try {
+        const [blogs,] = await db.execute("select * from blog where onay = 1")
+        const [categories, ]=await db.execute("select * from category");
+        res.render("users/blogs", {
+            title : "Tüm Kurslar",
+            blogs : blogs,
+            categories : categories
+
+        })
+    }
+    catch(err) {
+        console.log(err);
+
+    }
 
 
 });
 
+router.use("/", async function(req, res) {
+    try {
+        const [blogs,] = await db.execute("select * from blog where onay = 1")
+        const [categories, ]=await db.execute("select * from category");
+        res.render("users/index", {
+            title : "Popüler Kurslar",
+            blogs : blogs,
+            categories : categories
 
+        })
+    }
+    catch(err) {
+        console.log(err);
 
-router.use("/blogs", function (req, res) {
-    res.sendFile(path.join(__dirname, "../views/users", "blogs.html"));
+    }
 
-
+    
 });
 
-
-
-router.use("/", function (req, res, next) {
-    res.sendFile(path.join(__dirname, "../views/users", "index2.html"));
-
-
-});
-
-module.exports= router;
+module.exports = router;
