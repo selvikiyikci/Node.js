56be4d3b3621e5c51292c87cca6689faaa5d174b
selvikiyikci <selvikiyikci@gmail.com>
2024-07-25 00:38:57 +0300

    added Blog model definition with Sequelize and initial data sync

--- blogapp/models/blog.js
+++ blogapp/models/blog.js
@@ -2,50 +2,75 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../data/db");
 
 const Blog = sequelize.define("blog", {
-
-    blogid : {
-        type : DataTypes.INTEGER,
-        autoIncrement : true,
-        allowNull  : false,
-        primaryKey : true   
-
+    blogid: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     },
-    baslik : {
-        type : DataTypes.STRING,
-        allowNull : false,
+    baslik: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    altbaslik : {
-        type : DataTypes.TEXT,
-        allowNull : true
-
+    altbaslik: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    resim : {
-        type : DataTypes.STRING,
-        allowNull : false
+    aciklama: {
+        type: DataTypes.TEXT,
+        allowNull: true
     },
-    anasayfa : {
-        type : DataTypes.BOOLEAN,
-        allowNull : false
-
+    resim: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-
-    onay : {
-
-        type : DataTypes.BOOLEAN,
-        allowNull : false
+    anasayfa: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
     },
-    categoryid : {
-        type : DataTypes.INTEGER,
-        allowNull : false
+    onay: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    categoryid: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
-    
 });
 
-async function sync(){
-    await Blog.sync({ force : true});
+async function sync() {
+    await Blog.sync({ alter: true });
     console.log("blog tablosu eklendi");
+
+    const count = await Blog.count();
+
+    if(count == 0) { 
+        await Blog.create({
+            baslik: "Komple Uygulamalı Web Geliştirme Eğitimi",
+            altbaslik: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            aciklama: "Web geliştirme komple bir web sitesinin hem web tasarım (html,css,javascript), hem de web programlama (asp.net mvc) konularının kullanılarak geliştirilmesidir. Sadece html css kullanarak statik bir site tasarlayabiliriz ancak işin içine bir web programlama dilini de katarsak dinamik bir web uygulaması geliştirmiş oluruz.",
+            resim: "1.jpeg",
+            anasayfa: true,
+            onay: true,
+            categoryid: 1
+        });
+
+        await Blog.create({
+            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
+            altbaslik: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+            aciklama: "Python, son zamanların en popüler programlama dili haline geldi. Python' ın bu kadar popüler olmasındaki sebep şüphesiz öğrenmesi kolay bir yazılım dili olmasıdır.sadikturan adreslerinde paylaşmış olduğum python dersleri serisini takip ederek ister video ister yazılı kaynaklar yardımıyla kısa zamanda python programlama alanında uzmanlık kazanın ve hayal ettiğiniz projeyi gerçekleştirin.",
+            resim: "2.jpeg",
+            anasayfa: true,
+            onay: true,
+            categoryid: 1
+        });
     }
-    
-    sync();
-    
-module.exports =  Blog;
+
+
+}
+
+// migrations
+
+sync();
+
+module.exports = Blog;
