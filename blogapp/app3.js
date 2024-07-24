f6f2b4eaaec38647cf9092835a68983cde977b7a
selvikiyikci <selvikiyikci@gmail.com>
2024-07-25 00:37:00 +0300

    Added EJS view engine, static file serving, and route configuration

--- blogapp/app3.js
+++ blogapp/app3.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
+
+
 
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
