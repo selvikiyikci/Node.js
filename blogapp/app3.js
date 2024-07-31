842fef4d8cbf354372668418fef299a4f6760d69
selvikiyikci <selvikiyikci@gmail.com>
2024-07-31 13:44:59 +0300

    add routes, static file serving, and Sequelize setup with associations

--- blogapp/app3.js
+++ blogapp/app3.js
@@ -9,13 +9,30 @@ const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 
-
-
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(userRoutes); 
+
+const sequelize = require("./data/db");
+const dummyData = require("./data/dummy-data");
+const Category = require("./models/category");
+const Blog = require("./models/blog");
+
+// ilişkiler
+// one to many
+Blog.belongsToMany(Category, {through: "blogCategories"});
+Category.belongsToMany(Blog, {through: "blogCategories"});
+
+// uygulanması - sync
+
+// IIFE
+(async () => {
+    await sequelize.sync({ force: true });
+    await dummyData();
+})();
+
 
 app.listen(3000, function() {
     console.log("listening on port 3000");
