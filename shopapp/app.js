c6eb775781451f82fff3e50f4a4ccfb9f5c49c99
selvikiyikci <selvikiyikci@gmail.com>
2024-08-01 15:24:40 +0300

    added cors and restricted with get method

--- shopapp/app.js
+++ shopapp/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const products = require("./routes/products");
 const home = require("./routes/home");
 
 app.use(express.json());
+app.use(cors({
+    origin : "*",
+    methods : ["GET"]
+}));
+
+// app.use((req,res,next) => {
+//     res.setHeader("Acces-Control-Allow-Origin", "*");
+//     res.setHeader("Acces-Control-Allow-Methods","GET");
+//     next();
+// });
 
 app.use("/api/products" ,products);
 app.use("/", home);
