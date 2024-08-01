1f7e9475483f6b8de2a422a330071b744ee1aedd
selvikiyikci <selvikiyikci@gmail.com>
2024-08-01 11:27:33 +0300

    added slugfield link pages

--- blogapp/routes/user.js
+++ blogapp/routes/user.js
@@ -1,122 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/user");
 
+router.use("/blogs/category/:slug", userController.blogs_by_category);
 
+router.use("/blogs/:slug", userController.blogs_details);
 
-const Blog = require("../models/blog");
-const Category = require("../models/category");
+router.use("/blogs", userController.blog_list);
 
-const { Op }  = require("sequelize");
-
-router.use("/blogs/category/:categoryid", async function(req, res) {
-    const id = req.params.categoryid;
-    try {
-        const blogs = await Blog.findAll({
-                where : {
-                    categoryid : id,
-                    onay : true
-                },
-                raw : true
-            
-        })
-        const categories = await Category.findAll({raw : true});
-
-        res.render("users/blogs", {
-            title: "Tüm Kurslar",
-            blogs: blogs,
-            categories: categories,
-            selectedCategory: id
-        })
-    }
-    catch(err) {
-        console.log(err);
-    }
-});
-
-router.use("/blogs/:blogid", async function(req, res) {
-    const id = req.params.blogid;
-    try {
-        const blog = await Blog.findOne({
-            where : {
-                blogid : {     
-                [Op.eq] : id
-                }
-            },
-            raw : true
-
-        });
-
-
-        if(blog) {
-            return res.render("users/blog-details", {
-                title: blog.baslik,
-                blog: blog
-            });
-        }
-        res.redirect("/");
-    }
-    catch(err) {
-        console.log(err);
-    }
-});
-
-router.use("/blogs", async function(req, res) {
-    try {
-        const blogs = await Blog.findAll({
-            where : {
-                onay : {
-                    [Op.eq] : true //onay = 1
-                }
-             },
-              raw : true
-            
-        })
-        const categories = await Category.findAll({ raw : true });
-          
-
-        res.render("users/blogs", {
-            title: "Tüm Kurslar",
-            blogs: blogs,
-            categories: categories,
-            selectedCategory: null
-        })
-    }
-    catch(err) {
-        console.log(err);
-    }
-});
-
-router.use("/", async function(req, res) {
-    try {
-        const blogs = await Blog.findAll({
-            where: {
-                [Op.and]: [
-
-                   { anasayfa : true },
-                   { onay : true} 
-
-                ]
-            },
-            raw : true 
-        
-
-            
-        });
-        const categories  = await Category.findAll({
-            raw : true
-        });
-     
-        res.render("users/index", {
-            title: "Popüler Kurslar",
-            blogs: blogs,
-            categories: categories,
-            selectedCategory: null
-        })
-    }
-    catch(err) {
-        console.log(err);
-    }
-});
+router.use("/", userController.index);
  
 module.exports = router;
