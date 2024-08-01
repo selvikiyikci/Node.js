85426e0107d3e4fb74f5b96117ac797a42ac6426
selvikiyikci <selvikiyikci@gmail.com>
2024-08-01 11:36:09 +0300

    added sequelize and url

--- blogapp/models/category.js
+++ blogapp/models/category.js
@@ -2,41 +2,14 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../data/db");
 
 const Category = sequelize.define("category", {
-    categoryid: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
-}, {
-    timestamps: false
 });
-
-async function sync() {
-    await Category.sync({ alter: true });
-    console.log("category tablosu eklendi");
-
-    // await Category.create({ name: "Web Geliştirme" });
-    // await Category.create({ name: "Mobil Geliştirme" });
-    // await Category.create({ name: "Programlama" });
-
-    const count = await Category.count();
-
-    if(count == 0) { 
-        await Category.bulkCreate([
-            { name: "Web Geliştirme" },
-            { name: "Mobil Geliştirme" },
-            { name: "Programlama" }
-        ])
-    }
-
-    console.log("kategoriler eklendi");
-}
-
-sync();
 
 module.exports = Category;
