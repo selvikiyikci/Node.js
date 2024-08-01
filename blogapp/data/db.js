530c6df663b6f1a0cd0a2fc55644050a0f6dc9af
selvikiyikci <selvikiyikci@gmail.com>
2024-08-01 11:34:03 +0300

    mysql relationship

--- blogapp/data/db.js
+++ blogapp/data/db.js
@@ -1,36 +1,25 @@
-
 const config = require("../config");
 
-
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
-    dialect : "mysql",
-    host : config.db.host
+    dialect: "mysql",
+    host: config.db.host,
+    define: {
+        timestamps: false
+    }
 });
-async function connect(){
-    try { 
+
+async function connect() {
+    try {
         await sequelize.authenticate();
         console.log("mysql server bağlantısı yapıldı");
     }
     catch(err) {
-        console.log("bağlantı hatası", err);
-
+        console.log("bağlantı hatası ", err);
     }
 }
+
 connect();
 
-
 module.exports = sequelize;
-
-// let connection = mysql.createConnection(config.db);
-
-// connection.connect(function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-
-//     console.log("mysql server bağlantısı yapıldı");
-// });
-
-// module.exports = connection.promise();
-
